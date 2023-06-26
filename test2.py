66dcd0fa32ae672afe3a0ccb98dbb2be871064fd
jazxii <jassimshamim@gmail.com>
2023-06-26 14:31:49 +0530

    Branch data fetched and exported

--- test2.py
+++ test2.py
@@ -5,11 +5,14 @@ sys.path.append(PSSE_LOCATION)
 os.environ['PATH'] = os.environ['PATH'] + ';' +  PSSE_LOCATION 
 
 import psspy
-
+import pandas as pd
 #--------------------------------
 # PSS/E Saved case
 
 CASE = r"C:\Users\jassi\Downloads\KERALA_sld.sav"
+CASE1 = r"C:\Users\jassi\Downloads\KERALA_sld_100.sav"
+CASE2 = r"C:\Users\jassi\Downloads\KERALA_sld_400.sav"
+CASE3 = r"C:\Users\jassi\Downloads\KERALA_sld_700.sav"
 
 if __name__ == '__main__':
   psspy.psseinit(2000)
@@ -18,11 +21,146 @@ if __name__ == '__main__':
     options1=0, # disable tap stepping adjustment.
     options5=0, # disable switched shunt adjustment.
   )
-  
-# rarray = psspy.abrnreal(-1)
-# print("hi")
-# print(psspy.abrnreal(-1,1,1,1,1))  
-ierr, xarray = psspy.abrncplx(-1, 1, 1, 1, 1, 'RX')
-ierr, rarray = psspy.abrnreal(-1, 1, 1, 1, 1, 'CHARGING')
-for i in rarray:
-      print(i)
+ierr, count = psspy.abrncount(-1, 1, 1, 1, 1)
+ierr, [frombus] = psspy.abrnint(-1, 1, 1, 1, 1,'FROMNUMBER')
+ierr, [fromname] = psspy.abrnchar(-1, 1, 1, 1, 1,'FROMNAME')
+ierr, [tobus] = psspy.abrnint(-1, 1, 1, 1, 1,'TONUMBER')
+ierr, [toname] = psspy.abrnchar(-1, 1, 1, 1, 1,'TONAME')
+ierr, [Len] = psspy.abrnreal(-1, 1, 1, 1, 1, 'LENGTH')
+ierr, [p_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'P')
+ierr, [q_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'Q')
+ierr, [mva_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'MVA') 
+
+ierr, [tot_pload] = psspy.aareareal(-1,1,'PLOAD')
+ierr, [tot_qload] = psspy.aareareal(-1,1,'QLOAD') 
+ierr, [tot_pgen] = psspy.aareareal(-1,1,'PGEN')
+ierr, [tot_qgen] = psspy.aareareal(-1,1,'QGEN') 
+df = pd.DataFrame()
+df['FROM BUS'] = fromname
+df['FROMNAME'] = fromname
+df['TO BUS'] = tobus
+df['TONAME'] = toname
+df['Length'] = Len
+df['P'] = p_val
+df['Q'] = q_val
+df['MVA'] = mva_val
+df.to_csv('D:\Dev\PSSE Automation\data.csv',mode='a',index=False,header=False)
+print(df)
+
+print("total P load:",tot_pload[0])
+print("total Q load:",tot_qload[0])
+print("total P GEN:",tot_pgen[0])
+print("total Q GEN:",tot_qgen[0])
+
+
+psspy.case(CASE1)
+psspy.fnsl(
+    options1=0, # disable tap stepping adjustment.
+    options5=0, # disable switched shunt adjustment.
+  )
+ierr, count = psspy.abrncount(-1, 1, 1, 1, 1)
+ierr, [frombus] = psspy.abrnint(-1, 1, 1, 1, 1,'FROMNUMBER')
+ierr, [fromname] = psspy.abrnchar(-1, 1, 1, 1, 1,'FROMNAME')
+ierr, [tobus] = psspy.abrnint(-1, 1, 1, 1, 1,'TONUMBER')
+ierr, [toname] = psspy.abrnchar(-1, 1, 1, 1, 1,'TONAME')
+ierr, [Len] = psspy.abrnreal(-1, 1, 1, 1, 1, 'LENGTH')
+ierr, [p_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'P')
+ierr, [q_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'Q')
+ierr, [mva_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'MVA') 
+
+ierr, [tot_pload] = psspy.aareareal(-1,1,'PLOAD')
+ierr, [tot_qload] = psspy.aareareal(-1,1,'QLOAD') 
+ierr, [tot_pgen] = psspy.aareareal(-1,1,'PGEN')
+ierr, [tot_qgen] = psspy.aareareal(-1,1,'QGEN') 
+df1 = pd.DataFrame()
+df1['FROM BUS'] = fromname
+df1['FROMNAME'] = fromname
+df1['TO BUS'] = tobus
+df1['TONAME'] = toname
+df1['Length'] = Len
+df1['P'] = p_val
+df1['Q'] = q_val
+df1['MVA'] = mva_val
+df1.to_csv('D:\Dev\PSSE Automation\data1.csv',mode='a',index=False,header=False)
+print(df1)
+
+print("total P load:",tot_pload[0])
+print("total Q load:",tot_qload[0])
+print("total P GEN:",tot_pgen[0])
+print("total Q GEN:",tot_qgen[0])
+
+
+psspy.case(CASE2)
+psspy.fnsl(
+    options1=0, # disable tap stepping adjustment.
+    options5=0, # disable switched shunt adjustment.
+  )
+ierr, count = psspy.abrncount(-1, 1, 1, 1, 1)
+ierr, [frombus] = psspy.abrnint(-1, 1, 1, 1, 1,'FROMNUMBER')
+ierr, [fromname] = psspy.abrnchar(-1, 1, 1, 1, 1,'FROMNAME')
+ierr, [tobus] = psspy.abrnint(-1, 1, 1, 1, 1,'TONUMBER')
+ierr, [toname] = psspy.abrnchar(-1, 1, 1, 1, 1,'TONAME')
+ierr, [Len] = psspy.abrnreal(-1, 1, 1, 1, 1, 'LENGTH')
+ierr, [p_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'P')
+ierr, [q_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'Q')
+ierr, [mva_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'MVA') 
+
+ierr, [tot_pload] = psspy.aareareal(-1,1,'PLOAD')
+ierr, [tot_qload] = psspy.aareareal(-1,1,'QLOAD') 
+ierr, [tot_pgen] = psspy.aareareal(-1,1,'PGEN')
+ierr, [tot_qgen] = psspy.aareareal(-1,1,'QGEN') 
+df2 = pd.DataFrame()
+df2['FROM BUS'] = fromname
+df2['FROMNAME'] = fromname
+df2['TO BUS'] = tobus
+df2['TONAME'] = toname
+df2['Length'] = Len
+df2['P'] = p_val
+df2['Q'] = q_val
+df2['MVA'] = mva_val
+df2.to_csv('D:\Dev\PSSE Automation\data2.csv',mode='a',index=False,header=False)
+print(df2)
+
+print("total P load:",tot_pload[0])
+print("total Q load:",tot_qload[0])
+print("total P GEN:",tot_pgen[0])
+print("total Q GEN:",tot_qgen[0])
+
+
+psspy.case(CASE3)
+psspy.fnsl(
+    options1=0, # disable tap stepping adjustment.
+    options5=0, # disable switched shunt adjustment.
+  )
+ierr, count = psspy.abrncount(-1, 1, 1, 1, 1)
+ierr, [frombus] = psspy.abrnint(-1, 1, 1, 1, 1,'FROMNUMBER')
+ierr, [fromname] = psspy.abrnchar(-1, 1, 1, 1, 1,'FROMNAME')
+ierr, [tobus] = psspy.abrnint(-1, 1, 1, 1, 1,'TONUMBER')
+ierr, [toname] = psspy.abrnchar(-1, 1, 1, 1, 1,'TONAME')
+ierr, [Len] = psspy.abrnreal(-1, 1, 1, 1, 1, 'LENGTH')
+ierr, [p_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'P')
+ierr, [q_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'Q')
+ierr, [mva_val] = psspy.abrnreal(-1, 1, 1, 1, 1, 'MVA') 
+
+ierr, [tot_pload] = psspy.aareareal(-1,1,'PLOAD')
+ierr, [tot_qload] = psspy.aareareal(-1,1,'QLOAD') 
+ierr, [tot_pgen] = psspy.aareareal(-1,1,'PGEN')
+ierr, [tot_qgen] = psspy.aareareal(-1,1,'QGEN') 
+df3 = pd.DataFrame()
+df3['FROM BUS'] = fromname
+df3['FROMNAME'] = fromname
+df3['TO BUS'] = tobus
+df3['TONAME'] = toname
+df3['Length'] = Len
+df3['P'] = p_val
+df3['Q'] = q_val
+df3['MVA'] = mva_val
+df3.to_csv('D:\Dev\PSSE Automation\data3.csv',mode='a',index=False,header=False)
+print(df3)
+
+print("total P load:",tot_pload[0])
+print("total Q load:",tot_qload[0])
+print("total P GEN:",tot_pgen[0])
+print("total Q GEN:",tot_qgen[0])
+
+
--- test3_excel.py
+++ test3_excel.py
@@ -4,17 +4,10 @@ sys.path.append(PSSE_LOCATION)
 os.environ['PATH'] = os.environ['PATH'] + ';' +  PSSE_LOCATION 
 
 
-import excelpy
-x1 = excelpy.workbook(overwritesheet=True, mode='w') 
-x1.show()
-x1.set_cell('a1','Bus_Numbers')
-x1.set_cell('b1','voltage(pu)')
-x1.save('excel_1.xlsx')
-x1.close()
-x1.close_app()
-print ('done')
-# import psspy
-# import redirect
-# redirect.psse2py
-# psspy.psseinit(10000)
+import pandas as pd
+
+df = pd.read_csv('D:\Dev\PSSE Automation\data.csv')
+
+print(df) 
+
 
